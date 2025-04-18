6fc19c34f536e87b9f15a4ba7ed7bd3287128de8
kikewas <enrique.gomezgonzalez87@gmail.com>
2025-04-18 17:31:39 +0200

    Create setup.py
    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ordenes_trabajo_app',
+    version='0.0.1',
+    description='App para gestión de órdenes de trabajo',
+    author='kikewas',
+    author_email='kikewas@example.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
