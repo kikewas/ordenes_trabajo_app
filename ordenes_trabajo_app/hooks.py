c2f9cf957d248f50e85bc2dd9335a11f179187ec
kikewas <enrique.gomezgonzalez87@gmail.com>
2025-04-18 17:44:38 +0200

    Create hooks.py
    Create hooks.py

---
+++ ordenes_trabajo_app/hooks.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from . import __version__ as app_version
+
+app_name = "ordenes_trabajo_app"
+app_title = "Órdenes Trabajo App"
+app_publisher = "kikewas"
+app_description = "App para gestión de órdenes de trabajo"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "kikewas@example.com"
+app_license = "MIT"
